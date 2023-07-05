6000398c6926551686e8ebb9a791c79a2b5cedd8
SheepKeeper1990 <96897414+SheepKeeper1990@users.noreply.github.com>
2023-07-05 10:23:19 +0800

    Add files via upload

---
+++ frame_of_elliptic_plot2D_data.m
@@ -0,0 +1,45 @@
+%   The experimental part of the eye tracking method for depression based on local optical flow estimation in the paper outputs a projection of 
+%   a three-dimensional image, that is, a two-dimensional image, for easy observation of the distribution in the x and y directions.  
+%   Copyright Dr. Yang Li
+%   School of Basic, Huizhou Health Sciences and Polytechnic,
+%   Huizhou, China 516025
+%   $Revision: 1.0 $ 
+%   $Date: 2023/04/24 17:12:06 $
+%------------------------------------------------------------------------------
+
+clear;
+close all;
+clc;
+data = xlsread('data_line.csv'); 
+set(0,'defaultfigurecolor','w') 
+set(gcf,'position',[300,300,500,300]);
+
+scatter(data(:,5),data(:,1),20,'mp','filled')
+hold on
+scatter(data(:,5),data(:,3),20,'bo','filled')
+
+
+%…Ë÷√x÷·∑∂Œß∫ÕøÃ∂»
+set(gca,'XLim',[0 700]);
+set(gca,'XTick',[0:100:700]);
+xlabel('frame','Fontsize',10); 
+
+%…Ë÷√y÷·∑∂Œß∫ÕøÃ∂»
+set(gca,'YLim',[0 250]);
+set(gca,'YTick',[0:50:250]);
+ylabel('X & Y eye movement/pixel','Fontsize',10); 
+
+
+%%Y◊¯±Í
+hold on
+scatter(data(:,5),data(:,2),20,'gs','filled')
+hold on
+scatter(data(:,5),data(:,4),20,'r','filled')
+
+
+
+% Õº¿˝
+h1=legend('X of left eye','X of right eye','Y of left eye','Y of right eye','Location','North');
+
+set(h1,'Orientation','horizon','Box','off');
+
